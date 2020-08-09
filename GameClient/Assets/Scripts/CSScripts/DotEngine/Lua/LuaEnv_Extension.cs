3561f5eeb72363a02f9da7845833e663478cae5a
li.yubin <liyubin724@163.com>
2020-08-09 23:33:05 +0800

    添加Lua的调试工具，添加获取Lua内存工具

---
+++ GameClient/Assets/Scripts/CSScripts/DotEngine/Lua/LuaEnv_Extension.cs
@@ -0,0 +1,18 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+#endif
+
+
+namespace XLua
+{
+    public static class LuaEnv_Extension
+    {
+        public static int GetTotalMemory(this LuaEnv env)
+        {
+            int totalMemory = LuaAPI.lua_gc(env.L, LuaGCOptions.LUA_GCCOUNT, 0);
+            return totalMemory;
+        }
+    }
+}
