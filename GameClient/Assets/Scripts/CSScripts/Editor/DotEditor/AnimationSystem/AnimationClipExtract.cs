12465e7f345d395c39e6157a5f6590a098dcf56b
li.yubin <liyubin724@163.com>
2020-08-20 16:29:16 +0800

    添加美术素材；调整动画压缩的工具 1. 添加部分角色，带有动画，用于测试相关的内容 2. 调整FBX中动画导出及压缩工具 3. 添加仅针对AnimationClip的压缩工具

--- GameClient/Assets/Scripts/CSScripts/Editor/DotEditor/AnimationSystem/AnimationClipExtract.cs
+++ GameClient/Assets/Scripts/CSScripts/Editor/DotEditor/AnimationSystem/AnimationClipExtract.cs
@@ -1,6 +1,7 @@
 ﻿using DotEditor.Utilities;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityObject = UnityEngine.Object;
@@ -9,79 +10,96 @@ namespace DotEditor.AnimationSystem
 {
     public static class AnimationClipExtract
     {
-        [MenuItem("Game/Animation Clip/extract clips from fbx")]
-        public static void ExtractClipsFromFBX()
+        [MenuItem("Game/Animation Clip/compress clips")]
+        public static void CompressClip()
         {
-            GameObject[] gameObjects = Selection.GetFiltered<GameObject>(SelectionMode.Assets);
-            if(gameObjects == null || gameObjects.Length == 0)
+            AnimationClip[] selectedClips = Selection.GetFiltered<AnimationClip>(SelectionMode.Assets);
+            if (selectedClips == null || selectedClips.Length == 0)
             {
                 return;
             }
 
-            List<AnimationClip> clips = new List<AnimationClip>();
-            foreach(var go in gameObjects)
+            var clipInfos = (from clip in selectedClips
+                             let clipAssetPath = AssetDatabase.GetAssetPath(clip)
+                             let newClip = UnityObject.Instantiate<AnimationClip>(clip)
+                             select new { path = clipAssetPath, newClip = newClip }
+                             ).ToList();
+
+            clipInfos.ForEach(clipInfo =>
             {
-                string assetPath = AssetDatabase.GetAssetPath(go);
-                AnimationClip[] extractClips = ExtractClips(assetPath);
-                if (extractClips != null && extractClips.Length > 0)
-                {
-                    clips.AddRange(extractClips);
-                }
+                AssetDatabase.DeleteAsset(clipInfo.path);
+
+                AnimationClipCompress.RemoveScaleCurve(clipInfo.newClip);
+                AnimationClipCompress.CompressFloatPrecision(clipInfo.newClip, 4);
+
+                AssetDatabase.CreateAsset(clipInfo.newClip, clipInfo.path);
+                AssetDatabase.ImportAsset(clipInfo.path);
+            });
+
+            SelectionUtility.ActiveObjects((from clip in clipInfos select clip.newClip).ToArray()) ;
+        }
+
+        [MenuItem("Game/Animation Clip/extract clips from fbx")]
+        public static AnimationClip[] ExtractClipsFromFBX()
+        {
+            GameObject[] gameObjects = Selection.GetFiltered<GameObject>(SelectionMode.Assets);
+            if(gameObjects == null || gameObjects.Length == 0)
+            {
+                return null;
             }
 
+            var clips = (from go in gameObjects
+                         let assetPath = AssetDatabase.GetAssetPath(go)
+                         where Path.GetExtension(assetPath).ToLower() == ".fbx"
+                         let extractClips = FindClipFromFBX(assetPath)
+                         from clip in extractClips
+                         select ExtractClipFromFBX(clip)).ToList();
+
             SelectionUtility.ActiveObjects(clips.ToArray());
+
+            return clips.ToArray();
         }
 
         [MenuItem("Game/Animation Clip/extract and compress clips from fbx")]
         public static void ExtractAnCompressClipsFromFBX()
         {
-            GameObject[] gameObjects = Selection.GetFiltered<GameObject>(SelectionMode.Assets);
-            if (gameObjects == null || gameObjects.Length == 0)
+            AnimationClip[] extractClips = ExtractClipsFromFBX();
+            if(extractClips!=null && extractClips.Length>0)
             {
-                return;
+                CompressClip();
             }
-
-            List<AnimationClip> clips = new List<AnimationClip>();
-            foreach (var go in gameObjects)
-            {
-                string assetPath = AssetDatabase.GetAssetPath(go);
-                AnimationClip[] extractClips = ExtractClips(assetPath);
-
-                if (extractClips != null && extractClips.Length > 0)
-                {
-                    foreach(var clip in extractClips)
-                    {
-                        string clipAssetPath = AssetDatabase.GetAssetPath(clip);
-                        AnimationClip newClip = AnimationClipCompress.RemoveScaleCurve(clip, clipAssetPath);
-                        newClip = AnimationClipCompress.CompressFloatPrecision(newClip, clipAssetPath,4);
-
-                        clips.Add(newClip);
-                    }
-                }
-            }
-
-            SelectionUtility.ActiveObjects(clips.ToArray());
         }
 
-        private static AnimationClip[] ExtractClips(string assetPath)
+        private static AnimationClip ExtractClipFromFBX(AnimationClip clipInFBX)
         {
-            string assetName = Path.GetFileNameWithoutExtension(assetPath);
-            string dirName = Path.GetDirectoryName(assetPath);
+            string assetPath = AssetDatabase.GetAssetPath(clipInFBX);
 
+            string dirName = Path.GetDirectoryName(assetPath);
+            string assetName = Path.GetFileNameWithoutExtension(assetPath);
+            string clipAssetPath = $"{dirName}/{assetName}_{clipInFBX.name}.anim";
+
+            if (AssetDatabase.LoadAssetAtPath(clipAssetPath, typeof(UnityObject)) != null)
+            {
+                AssetDatabase.DeleteAsset(clipAssetPath);
+            }
+
+            AnimationClip newClip = UnityObject.Instantiate<AnimationClip>(clipInFBX);
+            AssetDatabase.CreateAsset(newClip, clipAssetPath);
+            AssetDatabase.ImportAsset(clipAssetPath);
+
+            return newClip;
+        }
+
+        private static AnimationClip[] FindClipFromFBX(string assetPath)
+        {
             List<AnimationClip> clips = new List<AnimationClip>();
             UnityObject[] objects = AssetDatabase.LoadAllAssetRepresentationsAtPath(assetPath);
             foreach(var obj in objects)
             {
                 AnimationClip clip = obj as AnimationClip;
-                if(clip!=null)
+                if (clip != null)
                 {
-                    AnimationClip newClip = UnityObject.Instantiate<AnimationClip>(clip);
-                    string newClipAssetPath = $"{dirName}/{assetName}_{clip.name}.anim";
-                    
-                    AssetDatabase.CreateAsset(newClip, newClipAssetPath);
-                    AssetDatabase.ImportAsset(newClipAssetPath);
-
-                    clips.Add(AssetDatabase.LoadAssetAtPath<AnimationClip>(newClipAssetPath));
+                    clips.Add(clip);
                 }
             }
 
