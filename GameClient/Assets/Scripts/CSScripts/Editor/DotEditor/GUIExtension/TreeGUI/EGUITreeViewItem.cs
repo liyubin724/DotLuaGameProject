81557b495bad8a46cf808960a89e770d46a007f6
li.yubin <liyubin724@163.com>
2020-09-25 20:20:15 +0800

    重新扩展TreeView实现可以动态展开元素

---
+++ GameClient/Assets/Scripts/CSScripts/Editor/DotEditor/GUIExtension/TreeGUI/EGUITreeViewItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor.IMGUI.Controls;
+
+namespace DotEditor.GUIExtension.TreeGUI
+{
+    public class EGUITreeViewItem : TreeViewItem
+    {
+        public TreeViewData Data { get; private set; }
+
+        public EGUITreeViewItem(TreeViewData data)
+        {
+            Data = data;
+
+            id = data.ID;
+            depth = data.Depth;
+            displayName = data.GetDisplayName();
+        }
+    }
+}
---
+++ GameClient/Assets/Scripts/CSScripts/Editor/DotEditor/GUIExtension/TreeGUI/Model/TreeViewData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DotEditor.GUIExtension.TreeGUI
+{
+    public class TreeViewData
+    {
+        internal int ID { get; set; } = -1;
+        internal int Depth { get; set; } = -1;
+
+        internal bool IsExpand { get; set; } = false;
+        internal TreeViewData Parent { get; set; } = null;
+        internal List<TreeViewData> Children { get;} = new List<TreeViewData>();
+
+        public virtual string GetDisplayName()
+        {
+            return GetType().Name;
+        }
+    }
+}
