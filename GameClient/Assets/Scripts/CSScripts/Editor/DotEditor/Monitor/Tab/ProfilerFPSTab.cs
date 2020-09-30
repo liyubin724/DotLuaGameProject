f9ed71f4c45f57e4dc4f7a0a230e5b39c969f3d1
li.yubin <liyubin724@163.com>
2020-09-30 16:26:55 +0800

    准备添加具体的各个Tab页的实现

---
+++ GameClient/Assets/Scripts/CSScripts/Editor/DotEditor/Monitor/Tab/ProfilerFPSTab.cs
@@ -0,0 +1,16 @@
+﻿using DotEngine.Monitor.Sampler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotEditor.Monitor
+{
+    public class ProfilerFPSTab : ProfilerTab
+    {
+        public ProfilerFPSTab() : base(ProfilerTabType.FPS)
+        {
+        }
+    }
+}
--- GameClient/Assets/Scripts/CSScripts/Editor/DotEditor/Monitor/Tab/ProfilerTab.cs
+++ GameClient/Assets/Scripts/CSScripts/Editor/DotEditor/Monitor/Tab/ProfilerTab.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using DotEngine.Monitor.Sampler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace DotEditor.Monitor
 {
-    class ProfilerTab
+    public enum ProfilerTabType
     {
+        FPS,
+        Log,
+        Device,
+        Memory,
+    }
+
+    public abstract class ProfilerTab
+    {
+        public ProfilerTabType TabType { get; private set; }
+
+        public ProfilerTab(ProfilerTabType type)
+        {
+            TabType = type;
+        }
+
+        public virtual void OnEnable()
+        {
+
+        }
+
+        public virtual void OnDisable()
+        {
+
+        }
+
+
+        public void OnGUI(Rect rect)
+        {
+
+        }
     }
 }
