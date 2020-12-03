cb65793101bd291d69b1fcd7821273433d94544f
li.yubin <liyubin724@163.com>
2020-12-03 16:54:19 +0800

    调整结构，未来将会以可插拔模块的方式实现对应的主体功能

---
+++ GameClient/Assets/Scripts/CSScripts/DotPackages/Dot.Core/Editor/UI/Internal/UIExtensionDefaultControls.cs
@@ -0,0 +1,198 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Reflection;
+using System.IO;
+using DotEngine.UI;
+//using DotEngine.Lua.UI;
+//using DotEngine.Lua.UI.Handler;
+
+namespace DotEditor.UI
+{
+    internal static class UIExtensionDefaultControls
+    {
+        public struct Resources
+        {
+            public Sprite standard;
+            public Sprite background;
+            public Sprite inputField;
+            public Sprite knob;
+            public Sprite checkmark;
+            public Sprite dropdown;
+            public Sprite mask;
+        }
+
+        private const float kWidth = 160f;
+        private const float kThickHeight = 30f;
+        private const float kThinHeight = 20f;
+        private static Vector2 s_ThickElementSize = new Vector2(kWidth, kThickHeight);
+        private static Vector2 s_ThinElementSize = new Vector2(kWidth, kThinHeight);
+        private static Vector2 s_ImageElementSize = new Vector2(100f, 100f);
+        private static Color s_DefaultSelectableColor = new Color(1f, 1f, 1f, 1f);
+        private static Color s_PanelColor = new Color(1f, 1f, 1f, 0.392f);
+        private static Color s_TextColor = new Color(50f / 255f, 50f / 255f, 50f / 255f, 1f);
+
+        // Helper methods at top
+
+        private static GameObject CreateUIElementRoot(string name, Vector2 size)
+        {
+            GameObject child = new GameObject(name);
+            RectTransform rectTransform = child.AddComponent<RectTransform>();
+            rectTransform.sizeDelta = size;
+            return child;
+        }
+
+        static GameObject CreateUIObject(string name, GameObject parent)
+        {
+            GameObject go = new GameObject(name);
+            go.AddComponent<RectTransform>();
+            SetParentAndAlign(go, parent);
+            return go;
+        }
+
+        private static void SetDefaultTextValues(Text lbl)
+        {
+            lbl.color = s_TextColor;
+
+            System.Type textType = lbl.GetType();
+            MethodInfo mi = textType.GetMethod("AssignDefaultFont", BindingFlags.NonPublic | BindingFlags.Instance);
+            mi.Invoke(lbl, new System.Object[] { });
+        }
+
+        private static void SetDefaultColorTransitionValues(Selectable slider)
+        {
+            ColorBlock colors = slider.colors;
+            colors.highlightedColor = new Color(0.882f, 0.882f, 0.882f);
+            colors.pressedColor = new Color(0.698f, 0.698f, 0.698f);
+            colors.disabledColor = new Color(0.521f, 0.521f, 0.521f);
+        }
+
+        private static void SetParentAndAlign(GameObject child, GameObject parent)
+        {
+            if (parent == null)
+                return;
+
+            child.transform.SetParent(parent.transform, false);
+            SetLayerRecursively(child, parent.layer);
+        }
+
+        private static void SetLayerRecursively(GameObject go, int layer)
+        {
+            go.layer = layer;
+            Transform t = go.transform;
+            for (int i = 0; i < t.childCount; i++)
+                SetLayerRecursively(t.GetChild(i).gameObject, layer);
+        }
+
+        public static GameObject CreateAtlasImage(Resources resources)
+        {
+            GameObject go = CreateUIElementRoot("AtlasImage", s_ImageElementSize);
+            go.AddComponent<UIAtlasImage>();
+            return go;
+        }
+
+        public static GameObject CreateAtlasImageAnimation(Resources resources)
+        {
+            GameObject go = CreateUIElementRoot("AtlasImageAnimation", s_ImageElementSize);
+            go.AddComponent<UIAtlasImageAnimation>();
+            return go;
+        }
+
+        public static GameObject CreateRawImage(Resources resources)
+        {
+            GameObject go = CreateUIElementRoot("RawImage", s_ImageElementSize);
+            go.AddComponent<UIRawImage>();
+            return go;
+        }
+
+        public static GameObject CreateBitmapText(Resources resources)
+        {
+            GameObject go = CreateUIElementRoot("BitmapText", s_ImageElementSize);
+            go.AddComponent<BitmapFontUIText>();
+            return go;
+        }
+
+        public static GameObject CreateLuaButton<T>(Resources resources) where T:Image
+        {
+            GameObject buttonRoot = CreateUIElementRoot("LuaButton", s_ThickElementSize);
+
+            GameObject childText = new GameObject("Text");
+            childText.AddComponent<RectTransform>();
+            SetParentAndAlign(childText, buttonRoot);
+
+            T image = buttonRoot.AddComponent<T>();
+            image.sprite = resources.standard;
+            image.type = Image.Type.Sliced;
+            image.color = s_DefaultSelectableColor;
+
+            Button bt = buttonRoot.AddComponent<Button>();
+            SetDefaultColorTransitionValues(bt);
+
+            //LuaButtonHandler handler = buttonRoot.AddComponent<LuaButtonHandler>();
+            //handler.button = bt;
+
+            Text text = childText.AddComponent<Text>();
+            text.text = "Lua Button";
+            text.alignment = TextAnchor.MiddleCenter;
+            SetDefaultTextValues(text);
+
+            RectTransform textRectTransform = childText.GetComponent<RectTransform>();
+            textRectTransform.anchorMin = Vector2.zero;
+            textRectTransform.anchorMax = Vector2.one;
+            textRectTransform.sizeDelta = Vector2.zero;
+
+            return buttonRoot;
+        }
+
+        public static GameObject CreateInputField(Resources resources)
+        {
+            GameObject root = CreateUIElementRoot("LuaInputField", s_ThickElementSize);
+
+            GameObject childPlaceholder = CreateUIObject("Placeholder", root);
+            GameObject childText = CreateUIObject("Text", root);
+
+            Image image = root.AddComponent<Image>();
+            image.sprite = resources.inputField;
+            image.type = Image.Type.Sliced;
+            image.color = s_DefaultSelectableColor;
+
+            InputField inputField = root.AddComponent<InputField>();
+            SetDefaultColorTransitionValues(inputField);
+
+            //LuaInputFieldHandler handler = root.AddComponent<LuaInputFieldHandler>();
+            //handler.inputField = inputField;
+
+            Text text = childText.AddComponent<Text>();
+            text.text = "";
+            text.supportRichText = false;
+            SetDefaultTextValues(text);
+
+            Text placeholder = childPlaceholder.AddComponent<Text>();
+            placeholder.text = "Enter text...";
+            placeholder.fontStyle = FontStyle.Italic;
+            // Make placeholder color half as opaque as normal text color.
+            Color placeholderColor = text.color;
+            placeholderColor.a *= 0.5f;
+            placeholder.color = placeholderColor;
+
+            RectTransform textRectTransform = childText.GetComponent<RectTransform>();
+            textRectTransform.anchorMin = Vector2.zero;
+            textRectTransform.anchorMax = Vector2.one;
+            textRectTransform.sizeDelta = Vector2.zero;
+            textRectTransform.offsetMin = new Vector2(10, 6);
+            textRectTransform.offsetMax = new Vector2(-10, -7);
+
+            RectTransform placeholderRectTransform = childPlaceholder.GetComponent<RectTransform>();
+            placeholderRectTransform.anchorMin = Vector2.zero;
+            placeholderRectTransform.anchorMax = Vector2.one;
+            placeholderRectTransform.sizeDelta = Vector2.zero;
+            placeholderRectTransform.offsetMin = new Vector2(10, 6);
+            placeholderRectTransform.offsetMax = new Vector2(-10, -7);
+
+            inputField.textComponent = text;
+            inputField.placeholder = placeholder;
+
+            return root;
+        }
+    }
+}
---
+++ GameClient/Assets/Scripts/CSScripts/DotPackages/Dot.Core/Runtime/Net/Server/ServerNetListener.cs
@@ -0,0 +1,267 @@
+﻿using DotEngine.Generic;
+using DotEngine.Net.Message;
+using DotEngine.Pool;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace DotEngine.Net.Server
+{
+    public interface IServerNetMessageHandler
+    {
+        ServerNetListener Listener { get; set; }
+        bool OnMessageHanlder(int netID, int messageID, object message);
+    }
+
+    public class ServerNetMessageData : IObjectPoolItem
+    {
+        public int netID = -1;
+        public int messageID = -1;
+        public object message = null;
+
+        public void OnGet()
+        {
+        }
+
+        public void OnNew()
+        {
+            
+        }
+
+        public void OnRelease()
+        {
+            netID = -1;
+            messageID = -1;
+            message = null;
+        }
+    }
+
+    public partial class ServerNetListener //: IDispose
+    {
+        private int uniqueID = -1;
+        public int UniqueID { get => uniqueID; }
+
+        private ManualResetEvent allDone = new ManualResetEvent(false);
+
+        private ItemObjectPool<ServerNetMessageData> dataPool = new ItemObjectPool<ServerNetMessageData>();
+        private object dataListLock = new object();
+        private List<ServerNetMessageData> dataList = new List<ServerNetMessageData>();
+
+        private Socket socket = null;
+        private UniqueIntID clientIDCreator = new UniqueIntID();
+
+        private object netDicLock = new object();
+        private Dictionary<int, ServerNet> netDic = new Dictionary<int, ServerNet>();
+
+        private Dictionary<string, IServerNetMessageHandler> messageHandlerDic = new Dictionary<string, IServerNetMessageHandler>();
+        private IMessageParser messageParser = null;
+        public ServerNetListener(int id,IMessageParser messageParser)
+        {
+            uniqueID = id;
+            this.messageParser = messageParser;
+        }
+
+        public void Startup(string ip,int port,int maxCount)
+        {
+            //IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            //IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            DebugLog.Info(NetConst.SERVER_LOGGER_TAG, $"ServerNetListener::Startup->address = {ipAddress.ToString()}");
+
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(maxCount);
+
+                Thread thread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        allDone.Reset();
+                        DebugLog.Info("ServerNet", "Waiting for a connection...");
+
+                        listener.BeginAccept(
+                            new AsyncCallback(AcceptCallback),
+                            listener);
+
+                        allDone.WaitOne();
+                    }
+                });
+                thread.Start();
+            }
+            catch (Exception e)
+            {
+                DebugLog.Info("ServerNet", e.ToString());
+            }
+        }
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            allDone.Set();
+
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            int netID = clientIDCreator.NextID;
+            ServerNet serverNet = new ServerNet(netID, handler,messageParser);
+            serverNet.MessageReceived = OnMessageReceived;
+            serverNet.NetDisconnected = OnNetDisconnected;
+
+            lock(netDicLock)
+            {
+                netDic.Add(netID, serverNet);
+            }
+        }
+
+        private void OnMessageReceived(int netID,int messageID,byte[] msgBytes)
+        {
+            object message = messageParser.DecodeMessage(messageID, msgBytes);
+
+            foreach(var kvp in messageHandlerDic)
+            {
+                if(kvp.Value.OnMessageHanlder(netID,messageID, message))
+                {
+                    return;
+                }
+            }
+            DebugLog.Error(NetConst.SERVER_LOGGER_TAG, $"ServerNetListener::OnMessageReceived->the handler not found.messageID = {messageID}");
+        }
+
+        private void OnNetDisconnected(int id)
+        {
+            lock(netDicLock)
+            {
+                if(netDic.ContainsKey(id))
+                {
+                    netDic.Remove(id);
+                }
+            }
+        }
+
+        public void DoUpdate(float deltaTime)
+        {
+        }
+
+        public void DoLateUpdate()
+        {
+            lock(netDicLock)
+            {
+                foreach(var kvp in netDic)
+                {
+                    kvp.Value.DoLateUpdate();
+                }
+            }
+        }
+
+        public void SendMessage(int messageID)
+        {
+            foreach(var kvp in netDic)
+            {
+                kvp.Value.SendMessage(messageID);
+            }
+        }
+
+        public void SendMessage(int netID,int messageID)
+        {
+            if (netDic.TryGetValue(netID, out ServerNet serverNet))
+            {
+                serverNet.SendMessage(messageID);
+            }
+        }
+
+        public void SendMessage(int messageID,object message)
+        {
+            foreach(var kvp in netDic)
+            {
+                kvp.Value.SendMessage(messageID, message);
+            }
+        }
+
+        public void SendMessage(int netID,int messageID,object message)
+        {
+            if (netDic.TryGetValue(netID, out ServerNet serverNet))
+            {
+                serverNet.SendMessage(messageID, message);
+            }
+        }
+
+        public void Dispose()
+        {
+            allDone.Set();
+            dataPool.Clear();
+            lock(dataListLock)
+            {
+                dataList.Clear();
+            }
+            lock(netDicLock)
+            {
+                foreach(var kvp in netDic)
+                {
+                    kvp.Value.Dispose();
+                }
+                netDic.Clear();
+            }
+
+            if (socket != null)
+            {
+                if (socket.Connected)
+                {
+                    try
+                    {
+                        socket.Shutdown(SocketShutdown.Both);
+                    }
+                    catch (Exception e)
+                    {
+                        DebugLog.Error(NetConst.SERVER_LOGGER_TAG, $"ServerNetListener::Disconnect->e = {e.Message}");
+                    }
+                    finally
+                    {
+                        socket.Close();
+                        socket = null;
+                    }
+                }
+                else
+                {
+                    socket.Close();
+                    socket = null;
+                }
+            }
+        }
+
+        #region Register Message Handler
+
+        public void RegisterMessageHandler(string name, IServerNetMessageHandler handler)
+        {
+            if (!messageHandlerDic.ContainsKey(name))
+            {
+                handler.Listener = this;
+                messageHandlerDic.Add(name, handler);
+            }
+            else
+            {
+                DebugLog.Error(NetConst.SERVER_LOGGER_TAG, $"ServerNetListener::RegisterMessageHandler->the handler has been added.name={name}");
+            }
+        }
+
+        public void UnregisterMessageHandler(string name)
+        {
+            if (messageHandlerDic.ContainsKey(name))
+            {
+                messageHandlerDic.Remove(name);
+            }
+            else
+            {
+                DebugLog.Error(NetConst.SERVER_LOGGER_TAG, $"ServerNetListener::UnregisterMessageHandler->The handler not found.name={name}");
+            }
+        }
+
+        #endregion
+    }
+}
