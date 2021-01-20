a66b8765f1bbf0951baee8147e2f2fd0290d3611
li.yubin <liyubin724@163.com>
2021-01-20 19:52:15 +0800

    重命名ListView的实现，同时添加ListView的简单绘制方法，同时添加拖动条实现

---
+++ GameClient/Assets/Scripts/CSScripts/DotPackages/Dot.GUIExt/Editor/DataGrid/EasyListView.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+using SystemObject = System.Object;
+
+namespace DotEditor.GUIExt.DataGrid
+{
+    public class EasyListView: EGUITreeView
+    {
+        private static float SEPARATOR_LINE_HEIGHT = 6.0f;
+
+        public bool ShowSeparator { get; set; } = true;
+
+        public Action<SystemObject> OnSelectedChange { get; set; }
+        public SystemObject SelectedItem
+        {
+            get
+            {
+                IList<int> selectedIDList = treeView.GetSelection();
+                if(selectedIDList!=null && selectedIDList.Count>0)
+                {
+                    return GetItemByID(selectedIDList[0]);
+                }
+
+                return null;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    treeView.SetSelection(new List<int>() { });
+                }else
+                {
+                    int id = GetIDByItem(value);
+                    if(id>=0)
+                    {
+                        treeView.SetSelection(new List<int>() { id }, TreeViewSelectionOptions.FireSelectionChanged);
+                    }
+                }
+            }
+        }
+
+        public EasyListView() : base()
+        {
+        }
+
+        public EasyListView(string[] displayNames,SystemObject[] datas):this()
+        {
+            for(int i =0;i<displayNames.Length;++i)
+            {
+                ViewModel.AddData(new GridViewData(displayNames[i], datas[i]));
+            }
+            Reload();
+        }
+
+        public void AddItem (string displayName,SystemObject data)
+        {
+            ViewModel.AddData(new GridViewData(displayName, data));
+            Reload();
+        }
+
+        public void InsertItem(int index,string displayName, SystemObject data)
+        {
+            ViewModel.InsertData(index, new GridViewData(displayName, data));
+            Reload();
+        }
+
+        public void AddItems(string[] displayNames,SystemObject[] datas)
+        {
+            if(datas!=null && datas.Length>0)
+            {
+                for (int i = 0; i < displayNames.Length; ++i)
+                {
+                    ViewModel.AddData(new GridViewData(displayNames[i], datas[i]));
+                }
+
+                Reload();
+            }
+        }
+
+        public void RemoveItem(SystemObject data)
+        {
+            GridViewData viewData = ViewModel.GetDataByUserdata(data);
+            if(viewData!=null)
+            {
+                ViewModel.RemoveData(viewData);
+
+                Reload();
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            var rootChilds = ViewModel.RootData.Children;
+            if(index>=0&&index < rootChilds.Count)
+            {
+                var item = rootChilds[index];
+                ViewModel.RemoveData(item);
+
+                Reload();
+            }
+        }
+
+        private SystemObject GetItemByID(int id)
+        {
+            var rootChilds = ViewModel.RootData.Children;
+            for (int i = 0; i < rootChilds.Count; ++i)
+            {
+                if (rootChilds[i].ID == id)
+                {
+                    return rootChilds[i].Userdata;
+                }
+            }
+
+            return null;
+        }
+
+        private int GetIDByItem(SystemObject data)
+        {
+            var rootChilds = ViewModel.RootData.Children;
+            for(int i =0;i<rootChilds.Count;++i)
+            {
+                if(rootChilds[i].Userdata == data)
+                {
+                    return rootChilds[i].ID;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Clear()
+        {
+            ViewModel.Clear();
+            Reload();
+        }
+
+        protected override void OnDrawRowItem(Rect rect, GridViewData itemData)
+        {
+            Rect drawRect = rect;
+            if(ShowSeparator)
+            {
+                drawRect.height -= SEPARATOR_LINE_HEIGHT;
+            }
+            EditorGUI.LabelField(drawRect, itemData.DisplayName);
+
+            if (ShowSeparator)
+            {
+                drawRect.y += drawRect.height;
+                drawRect.height = SEPARATOR_LINE_HEIGHT;
+                EGUI.DrawHorizontalLine(drawRect);
+            }
+        }
+
+        protected override float GetRowHeight(GridViewData itemData)
+        {
+            return base.GetRowHeight(itemData) + (ShowSeparator? SEPARATOR_LINE_HEIGHT : 0.0f);
+        }
+
+        protected override void OnItemSelectedChanged(GridViewData[] itemDatas)
+        {
+            if (itemDatas != null && itemDatas.Length > 0)
+            {
+                OnSelectedChange?.Invoke(itemDatas[0].Userdata);
+            }
+        }
+    }
+}
--- GameClient/Assets/Scripts/CSScripts/DotPackages/Dot.GUIExt/Editor/EditDrawerWindow.cs
+++ GameClient/Assets/Scripts/CSScripts/DotPackages/Dot.GUIExt/Editor/EditDrawerWindow.cs
@@ -3,6 +3,8 @@ using DotEditor.GUIExt.NativeDrawer;
 using DotEngine.Utilities;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using UnityEditor;
 using UnityEngine;
@@ -122,6 +124,12 @@ namespace DotEditor.GUIExt.EditDrawer
         }
 
         private ToolbarDrawer toolbarDrawer = null;
+        private EasyListViewDrawer listViewDrawer = null;
+        private Rect contentRect;
+        private string selectedFilePath = null;
+
+        private DragLineDrawer dragLineDrawer = null;
+        private bool isInited = false;
         private void OnGUI()
         {
             if(toolbarDrawer == null)
@@ -174,7 +182,56 @@ namespace DotEditor.GUIExt.EditDrawer
             }
             toolbarDrawer.OnGUILayout();
 
-            nativeObject.OnGUILayout();
+            if(listViewDrawer ==  null)
+            {
+                listViewDrawer = new EasyListViewDrawer()
+                {
+                    Text = "List View",
+                    DisplayNames = (from file in Directory.GetFiles(@"D:\", "*.*", SearchOption.TopDirectoryOnly) select Path.GetFileNameWithoutExtension(file)).ToArray(),
+                    Values = (from file in Directory.GetFiles(@"D:\", "*.*", SearchOption.TopDirectoryOnly) select file).ToArray(),
+                    OnValueChanged = (file) =>
+                    {
+                        selectedFilePath = (string)file;
+                    }
+                };
+            }
+            if(dragLineDrawer == null)
+            {
+                dragLineDrawer = new DragLineDrawer(this, DragLineDirection.Vertical);
+            }
+            Rect cRect = EditorGUILayout.GetControlRect(GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
+            if(cRect!=contentRect && cRect.width >10&&cRect.height>10)
+            {
+                contentRect = cRect;
+                dragLineDrawer.Position = new Rect(contentRect.x + 300, contentRect.y, 6, contentRect.height);
+            }
+            dragLineDrawer.LowLimitXY = 200;
+            dragLineDrawer.UpperLimitXY = contentRect.width * 0.8f;
+            dragLineDrawer.OnGUILayout();
+
+            Rect listViewRect = new Rect(contentRect.x, contentRect.y, dragLineDrawer.MinX - contentRect.x, contentRect.height);
+           // EGUI.DrawAreaLine(listViewRect, Color.blue);
+            GUILayout.BeginArea(listViewRect);
+            {
+                listViewDrawer.OnGUILayout();
+            }
+            GUILayout.EndArea();
+
+            Rect objectRect = new Rect(dragLineDrawer.MaxX, contentRect.y, contentRect.width - dragLineDrawer.MaxX, contentRect.height);
+          //  EGUI.DrawAreaLine(objectRect, Color.yellow);
+            GUILayout.BeginArea(objectRect);
+            {
+                if (string.IsNullOrEmpty(selectedFilePath))
+                {
+                    nativeObject.OnGUILayout();
+                }
+                else
+                {
+                    GUILayout.TextField(File.ReadAllText(selectedFilePath), GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
+                }
+            }
+            GUILayout.EndArea();
+
         }
     }
 }
