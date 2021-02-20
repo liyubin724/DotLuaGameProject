9e38914943970479aa8f461dcbbd4cc30026b3a4
li.yubin <liyubin724@163.com>
2021-02-20 19:43:38 +0800

    添加AB打包时与SBP配置参数

--- GameClient/Assets/Scripts/CSScripts/DotPackages/Dot.AAS/Editor/GeneratePreview/BundlePackerSetting.cs
+++ GameClient/Assets/Scripts/CSScripts/DotPackages/Dot.AAS/Editor/GeneratePreview/BundlePackerSetting.cs
@@ -1,4 +1,10 @@
-﻿namespace DotEditor.AAS
+﻿using DotEngine.AAS;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using UnityEditor;
+
+namespace DotEditor.AAS
 {
     public enum GenerateBuildTarget
     {
@@ -14,6 +20,38 @@
         public string outputFolder = string.Empty;
         
         public bool usedMd5ForBundle = false;
-        public string bundleDetailFileName = string.Empty;
+
+        private const string BUNDLE_PACKER_SETTING_FILE = "bundle_packer_setting.json";
+
+        public static BundlePackerSetting GetSetting()
+        {
+            BundlePackerSetting setting = null;
+            string settingFilePath = $"{BundleConst.GetBundleFolderInEditor()}/{BUNDLE_PACKER_SETTING_FILE}";
+            if(File.Exists(settingFilePath))
+            {
+                setting = JsonConvert.DeserializeObject<BundlePackerSetting>(File.ReadAllText(settingFilePath));
+            }
+            if(setting == null)
+            {
+                setting = new BundlePackerSetting();
+                if(EditorUserBuildSettings.activeBuildTarget == BuildTarget.StandaloneWindows)
+                {
+                    setting.target = GenerateBuildTarget.StandaloneWindows;
+                }else if(EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android)
+                {
+                    setting.target = GenerateBuildTarget.Android;
+                }else if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.iOS)
+                {
+                    setting.target = GenerateBuildTarget.iOS;
+                }else
+                {
+                    throw new Exception("Unsported");
+                }
+                setting.tempOutputFolder = $"{BundleConst.BUNDLE_FOLDER}/{setting.target.ToString()}/Temp";
+                setting.outputFolder = $"{BundleConst.BUNDLE_FOLDER}/{setting.target.ToString()}/Output";
+            }
+
+            return setting;
+        }
     }
 }
---
+++ GameClient/Assets/Scripts/CSScripts/DotPackages/Dot.AAS/Runtime/BundleConst.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DotEngine.AAS
+{
+    public static class BundleConst
+    {
+        public static readonly string BUNDLE_DETAIL_FILE_NAME = "bundle_detail";
+        public static readonly string BIN_FILE_EXT = ".bin";
+        public static readonly string TXT_FILE_EXT = ".txt";
+
+        public static readonly string BIN_BUNDLE_DETAIL_FILE = $"{BUNDLE_DETAIL_FILE_NAME}{BIN_FILE_EXT}";
+        public static readonly string TXT_BUNDLE_DETAIL_FILE = $"{BUNDLE_DETAIL_FILE_NAME}{TXT_FILE_EXT}";
+
+        public static readonly string BUNDLE_FOLDER = "AssetBundle";
+
+        public static string GetBundleFolder()
+        {
+            return string.Empty;
+        }
+
+        public static string GetBundleFolderInEditor()
+        {
+            return $"{Application.dataPath.Replace("/Assets", "")}/AssetBundle";
+        }
+    }
+}
+
