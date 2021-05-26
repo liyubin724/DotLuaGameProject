a83d937a6ffab7c43c181ed1136d70b9ecd6792e
li.yubin <liyubin724@163.com>
2021-05-26 22:58:03 +0800

    移动目录结构

--- GameTools/Config/DotEngine.WDB/IO/WDBExcelReader.cs
+++ GameTools/Config/DotEngine.WDB/IO/WDBExcelReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,21 @@ namespace DotEngine.WDB.IO
 {
     public class WDBExcelReader
     {
-        
+        public static WDBSheet[] ReadSheetFromFile(string excelDir)
+        {
+
+        }
+
+
+        private static bool IsExcelFile(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath) && !File.Exists(filePath))
+            {
+                return false;
+            }
+
+            string ext = Path.GetExtension(filePath);
+            return !string.IsNullOrEmpty(ext) && (ext == ".xls" || ext == ".xlsx");
+        }
     }
 }
