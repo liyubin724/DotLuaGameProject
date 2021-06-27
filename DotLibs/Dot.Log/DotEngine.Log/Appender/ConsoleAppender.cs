e7ba994aa20f7c932ebe959be0f50b5284f24287
li.yubin <liyubin724@163.com>
2021-06-28 00:43:27 +0800

    添加控制台日志的输出

---
+++ DotLibs/Dot.Log/DotEngine.Log/Appender/ConsoleAppender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DotEngine.Log
+{
+    public class ConsoleAppender : ALogAppender
+    {
+        public static readonly string NAME = "Console";
+
+        public ConsoleAppender() : base(NAME)
+        { }
+
+        protected override void OutputLogMessage(LogLevel level, string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
