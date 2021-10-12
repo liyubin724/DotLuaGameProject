41bf02ec2aa6fa8799208073a6d707829c3b6aae
li.yubin <liyubin724@163.com>
2021-10-13 00:46:14 +0800

    停止ECS的探究后，开始着手实现战斗系统

--- DotClient/Assets/Scripts/CSScripts/DotPackages/Dot.Core/Runtime/Extensions/UObjectExtension.cs
+++ DotClient/Assets/Scripts/CSScripts/DotPackages/Dot.Core/Runtime/Extensions/UObjectExtension.cs
@@ -4,6 +4,15 @@ namespace DotEngine.Core.Extensions
 {
     public static class UObjectExtension
     {
+        public static bool IsValid(this UnityObject obj)
+        {
+            if(obj != null && !obj.Equals(null))
+            {
+                return true;
+            }
+            return false;
+        }
+
         public static bool IsNull(this UnityObject obj)
         {
             if (obj == null || obj.Equals(null))
