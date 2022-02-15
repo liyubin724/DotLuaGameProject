d9a78f2868ba8ee5daad3d89c1ad14f30f8ff7b8
li.yubin <liyubin724@163.com>
2022-02-15 20:34:54 +0800

    添加空项目预留给数据处理

--- DotLibs/Config/DotTools.Config/Program.cs
+++ DotLibs/Config/DotTools.Config/Program.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using XLua;
 
-namespace DotTools.CodeGen
+namespace DotTools.Config
 {
-    public class LuaEngine
+    class Program
     {
-        public LuaEnv Env { get; private set; }
-
+        static void Main(string[] args)
+        {
+        }
     }
 }
