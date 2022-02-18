6e99e6f20f64226da0c1f84dec64300bc09636b0
li.yubin <liyubin724@163.com>
2022-02-18 13:23:39 +0800

    feat[Timer]:添加基于时间轮的定时器的实现;添加ID生成工具函数

---
+++ DotSource/DotDepends/DotEngine.Core/Generic/UniqueIDCreator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotEngine.Generic
+{
+    public interface IUniqueIDCreator<T>
+    {
+        T GetNextID();
+    }
+
+    public class GUIDCreator : IUniqueIDCreator<string>
+    {
+        public static GUIDCreator Default = new GUIDCreator();
+
+        private Dictionary<string, bool> guidDic = new Dictionary<string, bool>();
+        public GUIDCreator()
+        {
+        }
+
+        public string GetNextID()
+        {
+            Guid guid = Guid.NewGuid();
+            string strGuid = guid.ToString("N");
+            if (guidDic.ContainsKey(strGuid))
+            {
+                strGuid = GetNextID();
+            }
+
+            guidDic.Add(strGuid, true);
+
+            return strGuid;
+        }
+    }
+
+    public class IntIDCreator : IUniqueIDCreator<int>
+    {
+        public static IntIDCreator Default = new IntIDCreator(0);
+
+        private int currentId = 0;
+        public IntIDCreator(int start = 0)
+        {
+            currentId = start;
+        }
+
+        public int GetNextID()
+        {
+            return ++currentId;
+        }
+    }
+
+    public class LongIDCreator : IUniqueIDCreator<long>
+    {
+        public static LongIDCreator Default = new LongIDCreator(0);
+
+        private long currentId = 0;
+        public LongIDCreator(long start = 0)
+        {
+            currentId = start;
+        }
+
+        public long GetNextID()
+        {
+            return ++currentId;
+        }
+    }
+
+    public class SnowflakeIDCreator : IUniqueIDCreator<long>
+    {
+        public long GetNextID()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DotSource/DotDepends/DotEngine.Core/Timer/TimerTask.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+
+namespace DotEngine.Timer
+{
+    public enum TimerTaskCategory
+    {
+        None = 0,
+        Interval,
+        End,
+        IntervalAndEnd,
+    }
+
+    public class TimerTask
+    {
+        internal int Index { get; private set; } = -1;
+
+        private int m_IntervalInMS = 0;
+        private int m_TotalInMS = 0;
+        private TimerEventHandler m_OnIntervalEvent = null;
+        private TimerEventHandler m_OnEndEvent = null;
+        private object m_UserData = null;
+        private TimerTaskCategory m_Category = TimerTaskCategory.None;
+
+        internal int TriggerLeftInMS { get; set; } = 0;
+
+        public TimerTask()
+        {
+        }
+
+        internal void SetData(
+            int index,
+            float intervalInSec,
+            float totalInSec,
+            TimerEventHandler intervalCallback,
+            TimerEventHandler endCallback,
+            object userdata)
+        {
+            Index = index;
+
+            if(intervalInSec <=0 && totalInSec > 0)
+            {
+                m_Category = TimerTaskCategory.End;
+                m_TotalInMS = Mathf.RoundToInt(totalInSec * 1000);
+
+                TriggerLeftInMS = m_TotalInMS;
+            }else if(intervalInSec > 0 && totalInSec <= 0)
+            {
+                m_Category = TimerTaskCategory.Interval;
+                m_IntervalInMS = Mathf.RoundToInt(intervalInSec * 1000);
+
+                TriggerLeftInMS = m_IntervalInMS;
+            }else if(intervalInSec>0 && totalInSec > 0)
+            {
+                m_Category = TimerTaskCategory.IntervalAndEnd;
+                m_IntervalInMS = Mathf.RoundToInt(intervalInSec * 1000);
+                m_TotalInMS = Mathf.RoundToInt(totalInSec * 1000);
+
+                TriggerLeftInMS = m_IntervalInMS;
+            }else
+            {
+                DebugLog.Error("Timer error");
+            }
+            m_OnIntervalEvent = intervalCallback;
+            m_OnEndEvent = endCallback;
+            m_UserData = userdata;
+        }
+
+        internal bool Trigger()
+        {
+            if(TriggerLeftInMS <= 0)
+            {
+                if(m_Category == TimerTaskCategory.End)
+                {
+                    m_OnEndEvent?.Invoke(m_UserData);
+                    return true;
+                }else if(m_Category == TimerTaskCategory.Interval)
+                {
+                    m_OnIntervalEvent?.Invoke(m_UserData);
+                    TriggerLeftInMS = m_IntervalInMS;
+                    return false;
+                }else if(m_Category == TimerTaskCategory.IntervalAndEnd)
+                {
+                    m_OnIntervalEvent?.Invoke(m_UserData);
+                    m_TotalInMS -= m_IntervalInMS;
+                    if(m_TotalInMS <= 0)
+                    {
+                        m_OnEndEvent?.Invoke(m_UserData);
+                        return true;
+                    }else
+                    {
+                        if(m_TotalInMS >= m_IntervalInMS)
+                        {
+                            TriggerLeftInMS = m_IntervalInMS;
+                            return false;
+                        }else
+                        {
+                            m_Category = TimerTaskCategory.End;
+                            TriggerLeftInMS = m_TotalInMS;
+                            return false;
+                        }
+                    }
+                }else
+                {
+                    DebugLog.Error("Timer error");
+                    return true;
+                }
+            }else
+            {
+                return false;
+            }
+        }
+
+        internal void Reset()
+        {
+            Index = -1;
+            m_IntervalInMS = 0;
+            m_TotalInMS = 0;
+            m_OnEndEvent = null;
+            m_OnIntervalEvent = null;
+            m_UserData = null;
+            m_Category = TimerTaskCategory.None;
+            TriggerLeftInMS = 0;
+        }
+    }
+}
