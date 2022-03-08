1511aaf57a28c910dee32b15bb17bd6bec259f4e
li.yubin <liyubin724@163.com>
2022-03-08 20:20:07 +0800

    feat[Single]:添加单例基类

---
+++ DotSource/DotDepends/DotEngine.Core/Singleton.cs
@@ -0,0 +1,41 @@
+﻿namespace DotEngine
+{
+    public class Singleton<T> where T : Singleton<T>, new()
+    {
+        private static T instance = null;
+
+        public static T CreateInstance()
+        {
+            if (instance == null)
+            {
+                instance = new T();
+                instance.OnInit();
+            }
+            return instance;
+        }
+        public static T GetInstance()
+        {
+            return instance;
+        }
+
+        public static void DestroyInstance()
+        {
+            if (instance != null)
+            {
+                instance.OnDestroy();
+                instance = null;
+            }
+        }
+
+        protected Singleton()
+        { }
+
+        protected virtual void OnInit()
+        { }
+
+        protected virtual void OnDestroy()
+        {
+            instance = null;
+        }
+    }
+}
