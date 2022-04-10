2fb604720e48daf53733f79f3ad7b84291e27ca3
li.yubin <liyubin724@163.com>
2022-04-10 23:48:48 +0800

    optimize[core]:调整整体目录结构

---
+++ DotSource/DotDepends/DotCore/DotEngine.Core/Utilities/TypeUtility.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemObject = System.Object;
+
+namespace DotEngine.Utilities
+{
+    public static class TypeUtility
+    {
+        
+    }
+}
