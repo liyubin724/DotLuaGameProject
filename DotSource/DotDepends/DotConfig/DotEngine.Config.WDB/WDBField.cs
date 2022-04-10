25308b8831adf256e03e15575c71f7f36bdc6680
Erick <liyubin724@163.com>
2022-04-10 15:12:42 +0800

    optimize[config]:调整导出平台的值使用完全的字符串 (#23)

--- DotSource/DotDepends/DotConfig/DotEngine.Config.WDB/WDBField.cs
+++ DotSource/DotDepends/DotConfig/DotEngine.Config.WDB/WDBField.cs
@@ -46,7 +46,7 @@ namespace DotEngine.Config.WDB
             }
             set
             {
-                Platform = value.ToString("G").ToLower();
+                Platform = value.ToString().ToLower();
             }
         }
 
