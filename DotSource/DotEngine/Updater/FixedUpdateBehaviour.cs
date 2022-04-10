e7bbb73a3efeee06f2490577c992c3a883a74376
Erick <liyubin724@163.com>
2022-04-11 00:58:36 +0800

    feature[Engine]:调整Update相关的实现,简化侦听方式 (#27)

---
+++ DotSource/DotEngine/Updater/FixedUpdateBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace DotEngine.Updater
+{
+    public class FixedUpdateBehaviour : MonoBehaviour
+    {
+        public Action<float, float> Handler;
+
+        void FixedUpdate()
+        {
+            Handler?.Invoke(Time.fixedDeltaTime, Time.fixedUnscaledDeltaTime);
+        }
+    }
+}
--- DotSource/DotEngine/Updater/LateUpdateBehaviour.cs
+++ DotSource/DotEngine/Updater/LateUpdateBehaviour.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 namespace DotEngine.Updater
 {
-    public class UpdateBehaviour : MonoBehaviour
+    public class LateUpdateBehaviour : MonoBehaviour
     {
         public Action<float, float> Handler;
-        
-        void Update()
+
+        void LateUpdate()
         {
             Handler?.Invoke(Time.deltaTime, Time.unscaledDeltaTime);
         }
