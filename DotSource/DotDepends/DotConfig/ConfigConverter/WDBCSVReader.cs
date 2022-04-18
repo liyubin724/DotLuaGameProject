02a40ee6ee8366cf3a436d62eaf5fbec80f5dea9
li.yubin <liyubin724@163.com>
2022-04-18 19:05:45 +0800

    feature[config]:添加CSV的访问

--- DotSource/DotDepends/DotConfig/ConfigConverter/WDBCSVReader.cs
+++ DotSource/DotDepends/DotConfig/ConfigConverter/WDBCSVReader.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CsvHelper;
+using DotEngine.Config.WDB;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,27 @@ namespace DotTool.Config
 {
     public static class WDBCSVReader
     {
+        private static Action<LogType, string> sm_LogHandler;
+        private static WDBCSVStyle sm_Style;
+
+        public static WDBSheet[] ReadFromFile(string filePath, Action<LogType, string> logHandler, WDBCSVStyle style = null)
+        {
+            sm_LogHandler = logHandler;
+            sm_Style = style ?? new WDBCSVStyle();
+
+            if (string.IsNullOrEmpty(filePath) || !File.Exists(filePath))
+            {
+                return null;
+            }
+
+            List<WDBSheet> sheetList = new List<WDBSheet>();
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+
+                }
+            }
+        }
     }
 }
